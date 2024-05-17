2fe19b27547495ed4f7681d688eb6878de5fdf84
ushabandgar <ubandgar@birdzi.com>
2024-05-17 14:03:34 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Test1{
+	public static void main(String[] args){
+
+	int n=9;
+    if(n){
+        System.out.println("a");
+    }else
+  System.out.println("b");
+	}
+}
